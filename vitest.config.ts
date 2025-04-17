12753d8f1ad99fd2d8097d3371aea16f829bb803
Fortuné <fortunedev@gmail.com>
2025-04-18 00:36:06 +0200

    fix(test): correction de l'import de @vitejs/plugin-react dans vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { createRequire } from 'module'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const require = createRequire(import.meta.url)
+const react = require('@vitejs/plugin-react')
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react.default(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
